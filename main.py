310013cf857d82e84f38a0fc12b74533700eaec3
Guilherme Kowalczuk <guilhermekowalczuk@gmail.com>
2023-09-23 14:14:32 -0300

    Consistency checks in pylint and code structure

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import pandas as pd
 import re
-import time
-from functools import wraps
 
 
 def read_data_file(file_path: str) -> pd.DataFrame:
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding='utf-8') as f:
         raw_file = f.readlines()
 
     list_dados = [line.split() for line in raw_file]
     float_raw_lines = [list(map(float, raw_line)) for raw_line in list_dados]
-    return pd.DataFrame(float_raw_lines, columns=['lat', 'long', 'data_value'])
+    return pd.DataFrame(float_raw_lines, columns=['long', 'lat', 'data_value'])
 
 
 def read_contour_file(file_path: str) -> pd.DataFrame:
     line_split_comp = re.compile(r'\s*,')
 
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding='utf-8') as f:
         raw_file = f.readlines()
 
     l_raw_lines = [line_split_comp.split(raw_file_line.strip()) for raw_file_line in raw_file]
@@ -24,17 +22,18 @@ def read_contour_file(file_path: str) -> pd.DataFrame:
     float_raw_lines = [list(map(float, raw_line))[:2] for raw_line in l_raw_lines]
     header_line = float_raw_lines.pop(0)
     assert len(float_raw_lines) == int(header_line[0])
-    return pd.DataFrame(float_raw_lines, columns=['lat', 'long'])
+    return pd.DataFrame(float_raw_lines, columns=['long', 'lat'])
 
 
+# pylint: disable=unused-argument
 def apply_contour(contour_df: pd.DataFrame, data_df: pd.DataFrame) -> pd.DataFrame:
-    pass
+    return data_df
 
 
-def main() -> None:
-    contour_df: pd.DataFrame = read_contour_file('PSATCMG_CAMARGOS.bln')
-    data_df: pd.DataFrame = read_data_file('forecast_files/ETA40_p011221a021221.dat')
-    contour_df: pd.DataFrame = apply_contour(contour_df=contour_df, data_df=data_df)
+def main():
+    contour_df = read_contour_file('PSATCMG_CAMARGOS.bln')
+    data_df = read_data_file('forecast_files/ETA40_p011221a021221.dat')
+    data_df = apply_contour(contour_df=contour_df, data_df=data_df)
 
 
 if __name__ == '__main__':
